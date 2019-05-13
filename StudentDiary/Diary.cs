a66060b23ee05c95de3cc2c7653fa08620bd5897
MarcinSiennnicki <marcinsiennicki95@gmail.com>
2019-05-13 22:25:30 +0200

    next

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        List<float> ratings = new List<float>();
+
+        /// <summary>
+        /// dodawanie oceny
+        /// </summary>
+        /// <param name="rating"></param>
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+        /// <summary>
+        /// oblicznie sredniej
+        /// </summary>
+        /// <returns></returns>
+        public float AvgRating()
+        {
+            float sum = 0, avg = 0;
+            foreach (var item in ratings)
+            {
+                sum = +item;
+            }
+            avg = sum / ratings.Count();
+            return avg;
+        }
+        /// <summary>
+        /// wyswietla max
+        /// </summary>
+        /// <returns>max</returns>
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+        /// <summary>
+        /// wyswietla min
+        /// </summary>
+        /// <returns>Min</returns>
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+            //diary.AddRating(4.7f);
+
+            //float avg = diary.AvgRating();
+            //float max = diary.GiveMaxRating();
+            //float min = diary.GiveMinRating();
+            
+            for (; ; )
+            {
+                Console.WriteLine("Podaj ocene 1 : 10");
+
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);            
+
+                if (rating == 11)
+                {
+                    break;
+                }
+                if (result == true)
+                {
+                    if (rating > 0 && rating < 10)
+                    {
+                        diary.AddRating(rating);
+                    }
+                    else
+                        Console.WriteLine("Niepoprawna liczba");
+                }
+            }
+
+            Console.WriteLine("srednia twoich ocen " + diary.AvgRating());
+            Console.WriteLine("maxymalna ocena " + diary.GiveMaxRating());
+            Console.WriteLine("minimalna ocena" + diary.GiveMinRating());
+
+            Console.ReadKey();
+        }
+    }
+}
